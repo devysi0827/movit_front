96a9e0bb7fb126c2276f44f262280c881ce837e5
yeahaluu <yeahaluu@gmail.com>
2021-05-24 18:41:21 +0900

    real modal

--- src/views/mains/MovieCard.vue
+++ src/views/mains/MovieCard.vue
@@ -5,42 +5,17 @@
   <div class="card-body">
     <h3 class="card-title">{{ movie.title }}</h3>
     <img class="img-fluid" v-bind:src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path">
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-      Detail
-    </button>
-    <!-- <button @click="modalStart">Detail</button> -->
-    <!-- <p> class="card-text">{{ movie.overview }}</p> -->
   </div>  
 </div>
+  <div>
+    <b-button @click="modalShow = !modalShow">Open Modal</b-button>
+    <b-modal v-model="modalShow">
+      <h2> {{ movie.title}} </h2>
+      <img class="img-fluid" v-bind:src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path">
 
-<!-- @openmodal="modalTrue" -->
-<!-- <ModalView v-if="isModalViewed" @close-modal="isModalViewed = false">
-  <Content msg="안녕안녕"/>
-</ModalView> -->
-
-<!-- Modal -->
-<div class="b-modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>hello</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
+    </b-modal>
   </div>
 </div>
-</div>
-
 </template>
 
 <script>
@@ -59,14 +34,16 @@ export default {
   data: function() {
     return{
       // isModalViewed:false,
+      modalShow: false
+
     }
   },
-  // methods: {
-  //   modalStart: function () {
-  //     this.$emit('openmodal')
-  //     console.log(1)
-  //   },
-  // }
+  methods: {
+    // modalStart: function () {
+    //   this.$emit('openmodal')
+    //   console.log(1)
+    // },
+  }
 }
 </script>
 
